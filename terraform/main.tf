68d379abfcf2bf9a1b1ebe1e9cd0e076a879893f
johnpsasser <john.sasser@gmail.com>
2026-03-03 00:07:04 -0800

    Wire all modules together in root main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,6 +38,7 @@ provider "aws" {
   }
 }
 
+# --- Networking ---
 module "networking" {
   source       = "./modules/networking"
   project_name = var.project_name
@@ -45,7 +46,49 @@ module "networking" {
   aws_region   = var.aws_region
 }
 
+# --- Monitoring ---
 module "monitoring" {
-  source       = "./modules/monitoring"
+  source             = "./modules/monitoring"
+  project_name       = var.project_name
+  gitlab_instance_id = module.gitlab.instance_id
+}
+
+# --- GitLab ---
+module "gitlab" {
+  source            = "./modules/gitlab"
+  project_name      = var.project_name
+  vpc_id            = module.networking.vpc_id
+  subnet_id         = module.networking.private_subnet_ids[0]
+  security_group_id = module.networking.gitlab_security_group_id
+  instance_type     = var.instance_type
+  data_volume_size  = var.data_volume_size
+  domain_name       = var.domain_name
+  google_oauth_hd   = var.google_oauth_hd
+}
+
+# --- ALB ---
+module "alb" {
+  source             = "./modules/alb"
+  project_name       = var.project_name
+  vpc_id             = module.networking.vpc_id
+  subnet_ids         = module.networking.private_subnet_ids
+  security_group_id  = module.networking.alb_security_group_id
+  domain_name        = var.domain_name
+  route53_zone_id    = var.route53_zone_id
+  gitlab_instance_id = module.gitlab.instance_id
+
+  providers = {
+    aws             = aws
+    aws.dns_account = aws.dns_account
+  }
+}
+
+# --- DNS ---
+module "dns" {
+  source       = "./modules/dns"
   project_name = var.project_name
+  domain_name  = var.domain_name
+  vpc_id       = module.networking.vpc_id
+  alb_dns_name = module.alb.alb_dns_name
+  alb_zone_id  = module.alb.alb_zone_id
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,29 @@
-# Outputs populated as modules are added
+output "gitlab_instance_id" {
+  description = "GitLab EC2 instance ID"
+  value       = module.gitlab.instance_id
+}
+
+output "gitlab_private_ip" {
+  description = "GitLab EC2 private IP"
+  value       = module.gitlab.instance_private_ip
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS name"
+  value       = module.alb.alb_dns_name
+}
+
+output "gitlab_url" {
+  description = "GitLab URL"
+  value       = "https://${var.domain_name}"
+}
+
+output "backup_bucket" {
+  description = "S3 backup bucket name"
+  value       = module.gitlab.backup_bucket_name
+}
+
+output "ssm_connect_command" {
+  description = "Command to connect via SSM"
+  value       = "aws ssm start-session --target ${module.gitlab.instance_id}"
+}
